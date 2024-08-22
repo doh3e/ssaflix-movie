6d8441d85674c9ec4deb0c874e80f42b4e33ef70
Jieun Jung <wldms3333@gmail.com>
2024-08-22 10:26:09 +0900

    24.08.22 10:26 싸플릭스 프로젝트 완성

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 const nav = document.querySelector('nav');
-fetch('https://doh3e.github.io/web_project/navbar.html')
+fetch('../navbar.html')
 .then(res => res.text())
 .then(data => nav.innerHTML = data);
